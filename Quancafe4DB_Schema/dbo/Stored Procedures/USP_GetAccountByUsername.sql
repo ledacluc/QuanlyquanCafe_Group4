88c639ca157eba1ba31c647d1d666c37d00c7720
ledacluc <dangchiemhd2@gmail.com>
2025-06-01 15:43:04 +0700

    update main

--- Quancafe4DB_Schema/dbo/Stored Procedures/USP_GetAccountByUsername.sql
+++ Quancafe4DB_Schema/dbo/Stored Procedures/USP_GetAccountByUsername.sql
@@ -6,10 +6,3 @@ BEGIN
 END
 
 go
-create proc USP_Login
-@userName nvarchar(100), @passWord nvarchar(100)
-as
-begin
-		select *from dbo.Account where UserName = @userName and Password = @passWord
-end
-go
---
+++ Quancafe4DB_Schema/dbo/Stored Procedures/USP_Login.sql
@@ -0,0 +1,7 @@
+﻿create proc USP_Login
+@userName nvarchar(100), @passWord nvarchar(100)
+as
+begin
+		select *from dbo.Account where UserName = @userName and Password = @passWord
+end
+go
