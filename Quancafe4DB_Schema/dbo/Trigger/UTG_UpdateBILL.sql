114435cdafb36d5730965d0fde651d1b0bd08c9b
phucnguyen150606 <phucnguyen150606@gmail.com>
2025-06-18 22:24:09 +0700

    DemoPayment_and_Checkout_Table

---
+++ Quancafe4DB_Schema/dbo/Trigger/UTG_UpdateBILL.sql
@@ -0,0 +1,15 @@
+﻿CREATE TRIGGER UTG_UpdateBILL
+ON dbo.BILL FOR UPDATE
+AS
+BEGIN
+	DECLARE @idBILL INT
+	SELECT @idBILL = Id FROM inserted
+	DECLARE @IdTable INT
+	SELECT @IdTable = IdTable FROM dbo.BILL WHERE Id = @idBILL 
+	DECLARE @count INT = 0
+	SELECT @count = COUNT(*) FROM dbo.BILL WHERE Id = @IdTable AND Status = 0
+
+	IF(@count = 0)
+		UPDATE dbo.TableFood SET Status = N'TRONG' WHERE Id = @IdTable
+END
+GO
